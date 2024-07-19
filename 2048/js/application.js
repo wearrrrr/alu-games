2beae2ca136865d7a78ce42fea0f590c24371a1f
wearrrrr <contact@wearr.dev>
2024-07-19 18:54:31 -0500

    Fix 2048, add rooftop sniper, add tunnel rush, add basketball stars

--- 2048/js/application.js
+++ 2048/js/application.js
@@ -1,1 +1,4 @@
-animationDelay=100,minSearchTime=100,window.requestAnimationFrame((function(){new GameManager(4,KeyboardInputManager,HTMLActuator)}));
+// Wait till the browser is ready to render the game (avoids glitches)
+window.requestAnimationFrame(function () {
+  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+});
--- 2048/js/keyboard_input_manager.js
+++ 2048/js/keyboard_input_manager.js
@@ -1,1 +1,144 @@
-function KeyboardInputManager(){this.events={},this.listen()}KeyboardInputManager.prototype.on=function(e,t){this.events[e]||(this.events[e]=[]),this.events[e].push(t)},KeyboardInputManager.prototype.emit=function(e,t){var n=this.events[e];n&&n.forEach((function(e){e(t)}))},KeyboardInputManager.prototype.listen=function(){var e=this,t={38:0,39:1,40:2,37:3,75:0,76:1,74:2,72:3};document.addEventListener("keydown",(function(n){var r=n.altKey||n.ctrlKey||n.metaKey||n.shiftKey,i=t[n.which];r||(void 0!==i&&(n.preventDefault(),document.getElementById("feedback-container").innerHTML=" ",e.emit("move",i)),32===n.which&&e.restart.bind(e)(n))})),document.getElementsByClassName("retry-button")[0].addEventListener("click",this.restart.bind(this)),document.getElementById("hint-button").addEventListener("click",(function(t){t.preventDefault(),document.getElementById("feedback-container").innerHTML="<img src=img/spinner.gif />",e.emit("think")})),document.getElementById("run-button").addEventListener("click",(function(t){t.preventDefault(),e.emit("run")}));var n=[Hammer.DIRECTION_UP,Hammer.DIRECTION_RIGHT,Hammer.DIRECTION_DOWN,Hammer.DIRECTION_LEFT],r=document.getElementsByClassName("game-container")[0];Hammer(r,{drag_block_horizontal:!0,drag_block_vertical:!0}).on("swipe",(function(t){t.gesture.preventDefault(),mapped=n.indexOf(t.gesture.direction),-1!==mapped&&e.emit("move",mapped)}))},KeyboardInputManager.prototype.restart=function(e){e.preventDefault(),this.emit("restart")};
+function KeyboardInputManager() {
+  this.events = {};
+
+  if (window.navigator.msPointerEnabled) {
+    //Internet Explorer 10 style
+    this.eventTouchstart    = "MSPointerDown";
+    this.eventTouchmove     = "MSPointerMove";
+    this.eventTouchend      = "MSPointerUp";
+  } else {
+    this.eventTouchstart    = "touchstart";
+    this.eventTouchmove     = "touchmove";
+    this.eventTouchend      = "touchend";
+  }
+
+  this.listen();
+}
+
+KeyboardInputManager.prototype.on = function (event, callback) {
+  if (!this.events[event]) {
+    this.events[event] = [];
+  }
+  this.events[event].push(callback);
+};
+
+KeyboardInputManager.prototype.emit = function (event, data) {
+  var callbacks = this.events[event];
+  if (callbacks) {
+    callbacks.forEach(function (callback) {
+      callback(data);
+    });
+  }
+};
+
+KeyboardInputManager.prototype.listen = function () {
+  var self = this;
+
+  var map = {
+    38: 0, // Up
+    39: 1, // Right
+    40: 2, // Down
+    37: 3, // Left
+    75: 0, // Vim up
+    76: 1, // Vim right
+    74: 2, // Vim down
+    72: 3, // Vim left
+    87: 0, // W
+    68: 1, // D
+    83: 2, // S
+    65: 3  // A
+  };
+
+  // Respond to direction keys
+  document.addEventListener("keydown", function (event) {
+    var modifiers = event.altKey || event.ctrlKey || event.metaKey ||
+                    event.shiftKey;
+    var mapped    = map[event.which];
+
+    if (!modifiers) {
+      if (mapped !== undefined) {
+        event.preventDefault();
+        self.emit("move", mapped);
+      }
+    }
+
+    // R key restarts the game
+    if (!modifiers && event.which === 82) {
+      self.restart.call(self, event);
+    }
+  });
+
+  // Respond to button presses
+  this.bindButtonPress(".retry-button", this.restart);
+  this.bindButtonPress(".restart-button", this.restart);
+  this.bindButtonPress(".keep-playing-button", this.keepPlaying);
+
+  // Respond to swipe events
+  var touchStartClientX, touchStartClientY;
+  var gameContainer = document.getElementsByClassName("game-container")[0];
+
+  gameContainer.addEventListener(this.eventTouchstart, function (event) {
+    if ((!window.navigator.msPointerEnabled && event.touches.length > 1) ||
+        event.targetTouches.length > 1) {
+      return; // Ignore if touching with more than 1 finger
+    }
+
+    if (window.navigator.msPointerEnabled) {
+      touchStartClientX = event.pageX;
+      touchStartClientY = event.pageY;
+    } else {
+      touchStartClientX = event.touches[0].clientX;
+      touchStartClientY = event.touches[0].clientY;
+    }
+
+    event.preventDefault();
+  });
+
+  gameContainer.addEventListener(this.eventTouchmove, function (event) {
+    event.preventDefault();
+  });
+
+  gameContainer.addEventListener(this.eventTouchend, function (event) {
+    if ((!window.navigator.msPointerEnabled && event.touches.length > 0) ||
+        event.targetTouches.length > 0) {
+      return; // Ignore if still touching with one or more fingers
+    }
+
+    var touchEndClientX, touchEndClientY;
+
+    if (window.navigator.msPointerEnabled) {
+      touchEndClientX = event.pageX;
+      touchEndClientY = event.pageY;
+    } else {
+      touchEndClientX = event.changedTouches[0].clientX;
+      touchEndClientY = event.changedTouches[0].clientY;
+    }
+
+    var dx = touchEndClientX - touchStartClientX;
+    var absDx = Math.abs(dx);
+
+    var dy = touchEndClientY - touchStartClientY;
+    var absDy = Math.abs(dy);
+
+    if (Math.max(absDx, absDy) > 10) {
+      // (right : left) : (down : up)
+      self.emit("move", absDx > absDy ? (dx > 0 ? 1 : 3) : (dy > 0 ? 2 : 0));
+    }
+  });
+};
+
+KeyboardInputManager.prototype.restart = function (event) {
+  event.preventDefault();
+  this.emit("restart");
+};
+
+KeyboardInputManager.prototype.keepPlaying = function (event) {
+  event.preventDefault();
+  this.emit("keepPlaying");
+};
+
+KeyboardInputManager.prototype.bindButtonPress = function (selector, fn) {
+  var button = document.querySelector(selector);
+  button.addEventListener("click", fn.bind(this));
+  button.addEventListener(this.eventTouchend, fn.bind(this));
+};
